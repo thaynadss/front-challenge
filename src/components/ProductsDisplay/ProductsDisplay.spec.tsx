113863e7ccd768d409ad2069459eefe7f8230bd9
Thayna dos Santos Silva <thaynasilva@WN0173.local>
2022-02-18 11:16:42 -0300

    add more tests

--- src/components/ProductsDisplay/ProductsDisplay.spec.tsx
+++ src/components/ProductsDisplay/ProductsDisplay.spec.tsx
@@ -1,20 +1,30 @@
-import { render, RenderResult, screen, waitFor } from '@testing-library/react';
+import { render, RenderResult, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import { ProductsDisplay } from '.';
 import { CatalogContext } from '../../contexts/CatalogContext';
-import { Action } from '../../contexts/CatalogContext/reducer';
+import { Action, State } from '../../contexts/CatalogContext/reducer';
+import { api, getProducts } from '../../services/api';
+import axios from 'axios';
+import { apiMock } from '../../services/mock';
 
-const catalogState = {
+const emptyState = {
   filter: '',
   search: '',
 };
+
+const fullState = {
+  filter: 'vinho',
+  search: '',
+};
+
 const catalogDispatch = jest.fn() as React.Dispatch<Action>;
 
-const renderProductsDisplay = (): RenderResult => {
+const renderProductsDisplay = (state: State): RenderResult => {
   return render(
     <BrowserRouter>
       <CatalogContext.Provider value={{
-        catalogState: catalogState,
+        catalogState: state,
         catalogDispatch: catalogDispatch
       }}>
         <ProductsDisplay />
@@ -23,22 +33,91 @@ const renderProductsDisplay = (): RenderResult => {
   );
 };
 
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({})),
+  create: jest.fn(() => ({
+    get: jest.fn(() => Promise.resolve({}))
+  }))
+}));
+
+const mockedAxios = api as jest.Mocked<typeof axios>;
+
+
 describe('<ProductsDisplay />', () => {
-  it('should render a text and a gif while the products are not loaded', () => {
-    renderProductsDisplay();
+  describe('Render with empty state', () => {
+    beforeEach(async () => {
+      mockedAxios.get.mockResolvedValue({ data: apiMock });
+      const result = await getProducts(emptyState);
+      return result;
+    });
 
-    const gif = screen.getByAltText(/carregando/i);
-    const loadingTitle = screen.getByRole('heading', { name: /carregando/i });
+    afterEach(() => {
+      jest.clearAllMocks()
+    });
 
-    expect(loadingTitle).toBeInTheDocument();
-    expect(gif).toBeInTheDocument();
+    it('should render a text and a gif while the products are not loaded', () => {
+      renderProductsDisplay(emptyState);
+
+      const gif = screen.getByAltText(/carregando/i);
+      const loadingTitle = screen.getByRole('heading', { name: /carregando/i });
+
+      expect(loadingTitle).toBeInTheDocument();
+      expect(gif).toBeInTheDocument();
+    });
+
+    it('should not render a text and a gif if the api request completes', async () => {
+      renderProductsDisplay(emptyState);
+
+      await waitForElementToBeRemoved(() =>
+        screen.queryByAltText(/carregando/i)
+      );
+
+      await waitForElementToBeRemoved(() =>
+        screen.queryByRole('heading', { name: /carregando/i })
+      );
+    });
+
+    it('should render the quantity of items that were found and the products cards', async () => {
+      renderProductsDisplay(emptyState);
+
+      const quantityProducts = await screen.findByRole('heading', { name: /produtos encontrados/i });
+
+      expect(quantityProducts).toBeInTheDocument();
+
+      const cards = await screen.findAllByRole('heading', { name: /não sócio/i });
+
+      expect(cards[0]).toBeInTheDocument();
+    });
+
+    it('should not render clear search button if there are no searched products', async () => {
+      renderProductsDisplay(emptyState);
+
+      await waitFor(() =>
+        expect(screen.queryByRole('button', { name: /limpar pesquisa/i })).not.toBeInTheDocument()
+      );
+    });
   });
 
-  it('should render the quantity of items that were found and the products cards', async () => {
-    renderProductsDisplay();
+  describe('Render with full state', () => {
+    beforeEach(async () => {
+      mockedAxios.get.mockResolvedValue({ data: apiMock });
+      const result = await getProducts(fullState);
+      return result;
+    });
 
-    const quantityProducts = await screen.findByRole('heading', { name: /produtos encontrados/i });
+    afterEach(() => {
+      jest.clearAllMocks()
+    });
 
-    await waitFor(() => expect(quantityProducts).toBeInTheDocument());
+    it('should render clear search button if there are searched products and should call function to do that', async () => {
+      renderProductsDisplay(fullState);
+
+      const searchButton = await screen.findByRole('button', { name: 'Limpar pesquisa' });
+
+      await waitFor(() => expect(searchButton).toBeInTheDocument());
+      userEvent.click(searchButton);
+      expect(catalogDispatch).toHaveBeenCalledTimes(1);
+    });
+
   });
 });
